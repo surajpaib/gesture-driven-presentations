df952b343edd1bfe85919d9f375f1ed45ee213a0
Niklas Radtke <niklas.radtke@rwth-aachen.de>
2019-06-11 23:45:42 +0200

    Added creation of matrices for keypoints with interpolation

--- src/video_data.py
+++ src/video_data.py
@@ -1,5 +1,9 @@
 import xml.etree.ElementTree as ET
 
+import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
 from frame_data import FrameData
 
 class VideoData:
@@ -32,3 +36,65 @@ class VideoData:
     @property
     def fps(self):
         return self._framerate
+
+    def generate_matrices(self):
+        def sort_func(keypoint):
+            return keypoint[0]
+        matrix_list = []
+        last_keypoint_list = []
+        for i in range(len(self._frames)):
+            frame = self._frames[i]
+            matrix = np.zeros((64,64))
+            if i != 0:
+                matrix = matrix_list[i-1]
+            self.prep_mat(matrix)
+            for k in range(len(frame.keypoints[2:8])):
+                if (len(last_keypoint_list)) <= k:
+                    last_keypoint_list.append([])
+                last_keypoints = last_keypoint_list[k]
+                if(len(last_keypoints) > 3):
+                    last_keypoints.pop(0)
+                keypoint = (frame.keypoints[2:8])[k]
+                if keypoint[2] > 0.5:
+                    key_x = int(keypoint[0] * 16 + 32)
+                    key_y = int(keypoint[1] * 16 + 8)
+                    matrix[key_y,key_x] = 1
+                    last_keypoints.append([key_x,key_y])
+                else:
+                    last_keypoints.append(last_keypoints[-1])
+                if len(last_keypoints) > 1:
+                    #last_keypoints.sort(key=sort_func)
+                    last_keypoints_x = [p[0] for p in last_keypoints]
+                    last_keypoints_y = [p[1] for p in last_keypoints]
+                    if k == 5:
+                        print(last_keypoints)
+                    f1 = interp1d(last_keypoints_x,last_keypoints_y,kind='linear')
+                    steps =  max(last_keypoints_x) - min(last_keypoints_x) + 1
+                    step_size = 0.75/steps
+                    step = 0
+                    for x in range(min(last_keypoints_x) + 1, max(last_keypoints_x)):
+                        print(last_keypoints_x)
+                        print(min(last_keypoints_x))
+                        print(max(last_keypoints_x))
+                        matrix[int(f1(x)),x] = 0.25 + step*step_size
+                        step += 1
+                last_keypoint_list[k] = last_keypoints
+            matrix_list.append(matrix.copy())
+        return matrix_list
+
+    def prep_mat(self, frame):
+        for x in range(len(frame)):
+            for y in range(len(frame[x])):
+                if not frame[x][y] == 0:
+                    frame[x][y] -= 0.25
+                    if frame[x][y] < 0.25:
+                        frame[x][y] = 0
+
+if __name__ == "__main__":
+    data = VideoData()
+    data.load_xml_file("../process_results/output.xml")
+    matrix_list = data.generate_matrices()
+    for matrix in matrix_list[:9]:
+        plt.figure()
+        plt.imshow(matrix, cmap='gray')
+        plt.show()
