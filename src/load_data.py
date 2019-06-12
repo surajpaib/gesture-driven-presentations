3c14ab2bffed9f21d70a2312d75ea78b512ca1fb
Rodrigo Brechard <rodrigobrechard@gmail.com>
2019-06-12 17:48:46 +0200

    dilation done and matrix size as parameter

--- src/load_data.py
+++ src/load_data.py
@@ -1,12 +1,13 @@
 import os
 import pickle
 
+import cv2
 import numpy as np
 
 from video_data import VideoData
 
 
-def create_video_data_label(interpolation_frames, noise_parameters):
+def create_video_data_labels(interpolation_frames, noise_parameters, matrix_size, kernel_size=2):
     xml_folder = '../xml_files'
     data = []
     labels = []
@@ -17,9 +18,10 @@ def create_video_data_label(interpolation_frames, noise_parameters):
             file_path = xml_folder + '/' + folder + '/' + file
             video_data = VideoData(interpolations_frames=interpolation_frames, noise_frames=noise_parameters)
             video_data.load_xml_file(file_path)
-            matrix = video_data.generate_matrices()
+            matrix = video_data.generate_matrices(matrix_size)
             for frame in matrix:
-                data.append(frame)
+                kernel = np.ones((kernel_size, kernel_size), np.uint8)
+                data.append(cv2.dilate(frame, kernel, iterations=1))
                 labels.append(label)
             if matrix.shape[0] < min_data:
                 min_data = matrix.shape[0]
@@ -28,9 +30,9 @@ def create_video_data_label(interpolation_frames, noise_parameters):
     return np.array(data), np.array(labels)
 
 
-def load_video_data_labels(interpolation_frames, noise_parameters):
+def load_video_data_labels(interpolation_frames, noise_parameters, matrix_size=32):
     path = 'interpolation_' + str(interpolation_frames) + '_noise_' + str(
-        noise_parameters) + '.pkl'
+        noise_parameters) + '_matrix_size_' + str(matrix_size) + '.pkl'
     try:
         with open('../video_data_models/data_' + path, 'rb') as model:
             video_data = pickle.load(model)
@@ -42,7 +44,7 @@ def load_video_data_labels(interpolation_frames, noise_parameters):
 
     except:
         print("Failed to load video data or labels, create it")
-        video_data, video_labels = create_video_data_label(interpolation_frames, noise_parameters)
+        video_data, video_labels = create_video_data_labels(interpolation_frames, noise_parameters, matrix_size)
         with open('../video_data_models/data_' + path, 'wb') as output:
             pickle.dump(video_data, output)
         with open('../video_data_models/labels_' + path, 'wb') as output:
